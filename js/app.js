cfa6bccc5b17b3d394aa23086f3121788026fd2b
oleksandrlytvynenko08 <oleksandr.lytvynenko08@gmail.com>
2026-05-12 20:04:13 +0300

    Re-assignment 6

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
+var productsData = [
+  { id: 1, name: "Purple Stripe Garlic", desc: "Hardneck, rich and complex flavor", image: "images/garlic1.jpg" },
+  { id: 2, name: "Elephant Garlic", desc: "Large cloves, mild onion-like flavor", image: "images/garlic2.jpg" },
+  { id: 3, name: "Porcelain Garlic", desc: "Hardneck, sharp and pungent taste", image: "images/garlic3.jpg" },
+  { id: 4, name: "Artichoke Garlic", desc: "Softneck, easy to braid, mild flavor", image: "images/garlic4.jpg" },
+  { id: 5, name: "Silverskin Garlic", desc: "Softneck, long shelf life, strong taste", image: "images/garlic5.jpg" },
+  { id: 6, name: "Rocambole Garlic", desc: "Hardneck, deep full-bodied flavor", image: "images/garlic6.jpg" },
+];
+
 var app = new Vue({
   el: "#app",
   data: {
-    products: [
-      { id: 1, name: "TAG 1000 (TAG 853)", desc: "Tomato Determinate Red Standard Round", image: "images/tomato1.jpg" },
-      { id: 2, name: "TAG 1001 (TAG 855)", desc: "Tomato Determinate Red Standard Round", image: "images/tomato2.jpg" },
-      { id: 3, name: "TAG 1002 (TAG 809)", desc: "Tomato Determinate Red Standard Round", image: "images/tomato3.jpg" },
-    ],
+    products: productsData,
     cart: [],
+    selectedProduct: null,
     contactFields: {
       name: "",
       company: "",
@@ -28,6 +34,19 @@ var app = new Vue({
         return savedCartIds.includes(product.id);
       });
     },
+
+    addToCart: function (productId) {
+      var savedCartIds = JSON.parse(localStorage.getItem("cart")) || [];
+      if (!savedCartIds.includes(productId)) {
+        savedCartIds.push(productId);
+        localStorage.setItem("cart", JSON.stringify(savedCartIds));
+        this.getCart();
+        alert("Часник додано до кошика!");
+      } else {
+        alert("Цей товар вже у кошику!");
+      }
+    },
+
     removeFromCart: function (id) {
       this.cart = this.cart.filter(function (item) {
         return item.id !== id;
@@ -37,16 +56,35 @@ var app = new Vue({
       });
       localStorage.setItem("cart", JSON.stringify(newCartIds));
     },
+
     makeOrder: function () {
       this.orderSubmitted = true;
       this.cart = [];
       localStorage.removeItem("cart");
     },
+
+    selectProduct: function (id) {
+      localStorage.setItem("selectedProductId", id);
+      window.location.href = "garlic-one.html";
+    },
+
+    loadSelectedProduct: function () {
+      var selectedId = localStorage.getItem("selectedProductId");
+      if (selectedId) {
+        var foundProduct = this.products.find(function (p) {
+          return p.id == selectedId;
+        });
+        if (foundProduct) {
+          this.selectedProduct = foundProduct;
+        }
+      }
+    },
   },
   mounted: function () {
-    if (!localStorage.getItem("cart")) {
-      localStorage.setItem("cart", JSON.stringify([1, 2]));
-    }
     this.getCart();
+
+    if (window.location.pathname.includes("garlic-one.html")) {
+      this.loadSelectedProduct();
+    }
   },
 });
